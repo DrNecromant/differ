b19f789e140150ac2e3c8e2aae9784397e5a6a22
Dmitry <drnecromant@gmail.com>
2016-11-20 01:14:40 +0300

    Added unit tests for DB
    Testing record creation for Data and Diff tables

--- unittests.py
+++ unittests.py
@@ -1,12 +1,46 @@
 import unittest
 
-from app import app
+from app import app, db, Data, Diff
 from flask_api import status
 from consts import *
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db.create_all()
+
+# Create pool of IDs for testing
+ids = range(1, 100)
+
+class TestDB(unittest.TestCase):
+	"""
+	Unittests for db
+	"""
+	def setUp(self):
+		pass
+
+	def testData(self):
+		"""
+		Try to create record in a table Data
+		"""
+		data = Data(task_id = ids.pop(), side = "left")
+		db.session.add(data)
+		data.data = "somedata"
+		db.session.commit()
+
+	def testDiff(self):
+		"""
+		Try to create record in a table Diff
+		"""
+		diff = Diff(task_id = ids.pop(), offset = 10, length = 30)
+		db.session.add(diff)
+		db.session.commit()
+
+	def tearDown(self):
+		pass
+
 class TestEndpoints(unittest.TestCase):
 	"""
-	Unittests for differ
+	Unittests for endpoints
 	"""
 	def setUp(self):
 		self.client = app.test_client()
@@ -31,8 +65,8 @@ class TestEndpoints(unittest.TestCase):
 		Try to get difference if there is no files for task
 		Returns 404 for task URL
 		"""
-		self.task = 1
-		res = self.client.get() 
+		self.task = ids.pop()
+		res = self.client.get(self.getTaskUrl())
 		self.assertEquals(res.status_code, status.HTTP_404_NOT_FOUND)
 
 	def testAddEmptyFile(self):
@@ -40,7 +74,7 @@ class TestEndpoints(unittest.TestCase):
 		Try to send json without "data" key
 		Returns 400 for file url
 		"""
-		self.task = 2
+		self.task = ids.pop()
 		res = self.client.put(self.getLeftFileUrl(), data = '{}')
 		self.assertEquals(res.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -49,7 +83,7 @@ class TestEndpoints(unittest.TestCase):
 		Try to add files to compare
 		Returns 201 for file urls
 		"""
-		self.task = 3
+		self.task = ids.pop()
 		res = self.client.put(self.getLeftFileUrl(), data = '{"data": "filedata"}')
 		self.assertEquals(res.status_code, status.HTTP_201_CREATED)
 		res = self.client.put(self.getRightFileUrl(), data = '{"data": "filedata"}')
@@ -60,7 +94,7 @@ class TestEndpoints(unittest.TestCase):
 		Try to add file different from left or right url
 		Returns 404 for fake file url
 		"""
-		self.task = 4
+		self.task = ids.pop()
 		res = self.client.put(self.getFakeFileUrl(), data = '{"data": "filedata"}')
 		self.assertEquals(res.status_code, status.HTTP_404_NOT_FOUND)
 
@@ -69,7 +103,7 @@ class TestEndpoints(unittest.TestCase):
 		Try to run comparison task with one file only
 		Returns 505 for task url
 		"""
-		self.task = 5
+		self.task = ids.pop()
 		res = self.client.put(self.getRightFileUrl(), data = '{"data": "filedata"}')
 		res = self.client.get(self.getTaskUrl())
 		self.assertEquals(res.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -79,7 +113,7 @@ class TestEndpoints(unittest.TestCase):
 		Try to diff existed files
 		Returns 200 for task url
 		"""
-		self.task = 6
+		self.task = ids.pop()
 		res = self.client.put(self.getLeftFileUrl(), data = '{"data": "filedata1"}')
 		res = self.client.put(self.getRightFileUrl(), data = '{"data": "filedata2"}')
 		res = self.client.get(self.getTaskUrl())
@@ -90,5 +124,10 @@ class TestEndpoints(unittest.TestCase):
 		pass
 
 if __name__ == "__main__":
-	suite = unittest.TestLoader().loadTestsFromTestCase(TestEndpoints)
-	unittest.TextTestRunner(verbosity = 2).run(suite)
+	SuiteDB = unittest.TestLoader().loadTestsFromTestCase(TestDB)
+	SuiteEndpoints = unittest.TestLoader().loadTestsFromTestCase(TestEndpoints)
+
+	for suite in (SuiteDB, SuiteEndpoints):
+		print "=" * 70
+		unittest.TextTestRunner(verbosity = 2).run(suite)
+		print
