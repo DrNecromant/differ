da7845ab9ef9d48f760f6d54c5f486880dbeae46
Dmitry <drnecromant@gmail.com>
2016-11-19 16:32:26 +0300

    Add unittests skeleton

---
+++ unittests.py
@@ -0,0 +1,16 @@
+import unittest
+
+from consts import *
+
+class TestDiffer(unittest.TestCase):
+	"""
+	Unittests for differ
+	"""
+	def setUp(self):
+		pass
+	
+	def tearDown(self):
+		pass
+
+if __name__ == "__main__":
+	unittest.main()
