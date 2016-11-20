b82e5c5928aac29d7b71a0e4d4b9d07fa771848a
Dmitry <drnecromant@gmail.com>
2016-11-20 20:55:45 +0300

    Add skeleton for integration tests

---
+++ integrationtests.py
@@ -0,0 +1,18 @@
+import os
+import unittest
+import base64
+
+from testdata import *
+
+class TestDiffer(unittest.TestCase):
+	"""
+	Integration test for differ app
+	"""
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
